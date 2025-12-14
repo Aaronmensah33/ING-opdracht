ff262382bcccc034042ef2e12f704ca9dbb9e57a
Aaron Mensah <103023593+Aaronmensah33@users.noreply.github.com>
2025-12-15 00:46:07 +0100

    [Added] Zebra class extending Herbivore with specific behaviors

--- src/com/ing/zoo/Zebra.java
+++ src/com/ing/zoo/Zebra.java
@@ -1,26 +1,17 @@
 package com.ing.zoo;
 
-import java.util.Random;
-
-public class Zebra {
-    public String name;
-    public String helloText;
-    public String eatText;
-    public String trick;
-
-    public Zebra()
-    {
+public class Zebra extends Herbivore {
+    public Zebra(String name) {
+        super(name);
     }
 
-    public void sayHello()
-    {
-        helloText = "zebra zebra";
-        System.out.println(helloText);
+    @Override
+    public void sayHello() {
+        System.out.println(name + " zebra zebra");
     }
 
-    public void eatLeaves()
-    {
-        eatText = "munch munch zank yee bra";
-        System.out.println(eatText);
+    @Override
+    public void eatLeaves() {
+        System.out.println(name + " munch munch zebra stripes");
     }
 }
