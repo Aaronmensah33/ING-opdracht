cbde82233c94a1ffead721261398eb47ca6f6686
Aaron Mensah <103023593+Aaronmensah33@users.noreply.github.com>
2025-12-15 00:45:17 +0100

    [Added] Pig class extending Herbivore with specific behaviors

--- src/com/ing/zoo/Pig.java
+++ src/com/ing/zoo/Pig.java
@@ -2,46 +2,36 @@ package com.ing.zoo;
 
 import java.util.Random;
 
-public class Pig {
-    public String name;
-    public String helloText;
-    public String eatText;
-    public String trick;
-
-    public Pig()
+public class Pig extends Herbivore{
+    public Pig(String name)
     {
+        super(name);
     }
 
+    @Override
     public void sayHello()
     {
-        helloText = "splash";
-        System.out.println(helloText);
+        System.out.println(name + " oink oink");
     }
 
+    @Override
     public void eatLeaves()
     {
-        eatText = "munch munch oink";
-        System.out.println(eatText);
+        System.out.println(name + " munch munch oink");
     }
 
-    public void eatMeat()
-    {
-        eatText = "nomnomnom oink thx";
-        System.out.println(eatText);
-    }
-
+    @Override
     public void performTrick()
     {
         Random random = new Random();
         int rnd = random.nextInt(2);
         if(rnd == 0)
         {
-            trick = "rolls in the mud";
+            System.out.println(name + " rolls in the mud");
         }
         else
         {
-            trick = "runs in circles";
+            System.out.println(name + " runs in circles");
         }
-        System.out.println(trick);
     }
 }
